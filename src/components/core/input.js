f4876973e30e9039e888e70de8b941f68eb2f1fc
Jilver <jilverrivera1@gmail.com>
2022-02-04 18:05:08 -0500

    feat/core components

--- src/components/core/input.js
+++ src/components/core/input.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {TextInput, Platform} from 'react-native';
-import {COLORS} from '../../constants';
-import {coreStyles} from '../../styles';
+import React, {useContext} from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+import {FONTS, SIZES} from '../../constants';
+import {ThemeContext} from '../../context/ThemeContext';
 
 const Input = ({
   keyboardType,
@@ -9,16 +9,35 @@ const Input = ({
   onChange,
   valueType,
   capitalize,
+  placeholder,
 }) => {
+  const {Colors} = useContext(ThemeContext);
   return (
     <TextInput
-      style={[coreStyles.input, Platform.OS === 'ios' && coreStyles.inputIOS]}
+      style={{
+        ...styles.input,
+        backgroundColor: Colors.primary,
+        color: Colors.secondary,
+      }}
       keyboardType={keyboardType}
       secureTextEntry={secureEntry}
       autoCapitalize={capitalize}
       onChangeText={value => onChange(value, valueType)}
-      underlineColorAndroid={COLORS.gray}
+      placeholder={placeholder}
+      placeholderTextColor={Colors.gray}
     />
   );
 };
 export default Input;
+
+const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+    fontSize: SIZES.regular,
+    fontFamily: FONTS.regular,
+  },
+});
