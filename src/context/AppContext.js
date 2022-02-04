e5ed92d9559b2980215c83ec53d3921508260f53
Jilver <jilverrivera1@gmail.com>
2022-02-04 17:57:12 -0500

    feat/application context

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,36 +1,130 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useContext, useEffect, useState} from 'react';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+import {AuthContext} from './AuthContext';
+import {Alert} from 'react-native';
+import {loadAnimals} from '../services/loadAnimals';
+
+const animalState = {
+  sex: 'male',
+  born: false,
+  purchased: false,
+  sold: false,
+  uri: null,
+  filename: null,
+};
 
 export const AppContext = createContext();
 
 export const AppProvider = ({children}) => {
-  const [selected, setSelected] = useState('animals');
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const {userState} = useContext(AuthContext);
+  const {userID} = userState;
 
-  // <----- add new configuration ------>
-  const [boughtAnimal, setBoughtAnimal] = useState(false);
-  const [bornAnimal, setBornAnimal] = useState(false);
+  const [animals, setAnimals] = useState(null);
+  const [animal, setAnimal] = useState(animalState);
+  const [categories, setCategories] = useState(0);
 
-  const handleAnimal = (isBought = '') => {
-    if (isBought === 'bought') {
-      setBoughtAnimal(!boughtAnimal);
-      setBornAnimal(false);
-    } else {
-      setBornAnimal(!bornAnimal);
-      setBoughtAnimal(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+
+  //   // <----- QUERYS ------>
+  useEffect(() => {
+    getAnimals();
+  }, [userID]);
+
+  //   // <----- FIREBASE FUNCTIONS ------>
+  const getAnimals = async () => {
+    if (userID) {
+      const animals = await loadAnimals(userID);
+      setAnimals(animals);
     }
   };
 
-  const state = {
-    buttons: {selected, setSelected},
-    animalCard: {currentIndex, setCurrentIndex},
+  const uploadImage = async () => {
+    let animalPath = `${userID}/animals/${animal.filename}`;
+    await storage().ref(animalPath).putFile(animal.uri);
+  };
 
-    addNew: {
-      boughtAnimal,
-      setBoughtAnimal,
-      bornAnimal,
-      setBornAnimal,
-      handleAnimal,
+  const newAnimal = async data => {
+    await firestore()
+      .collection(`Users/${userID}/animals`)
+      .add({...data})
+      .then(() => {
+        Alert.alert(
+          'Animal añadido',
+          'Tu animal fue añadido satisfactoriamente.',
+        );
+      })
+      .catch(() =>
+        Alert.alert(
+          'Error',
+          'Surgio un pequeño inconveniente, vuelve a intentarlo de nuevo.',
+        ),
+      );
+
+    if (animal.uri) {
+      uploadImage();
+    }
+    setAnimal(animalState);
+  };
+
+  const newProfit = async data => {
+    await firestore()
+      .collection(`Users/${userID}/profits`)
+      .add({...data})
+      .then(() => {
+        Alert.alert(
+          'Mensaje',
+          'Tu ingreso ha sido añadido satisfactoriamente.',
+        );
+      })
+      .catch(() =>
+        Alert.alert(
+          'Error',
+          'Surgio un pequeño inconveniente, vuelve a intentarlo de nuevo.',
+        ),
+      );
+  };
+
+  const newExpense = async data => {
+    await firestore()
+      .collection(`Users/${userID}/expenses`)
+      .add({...data})
+      .then(() => {
+        Alert.alert(
+            'Mensaje',
+            'Tu gasto ha sido añadido satisfactoriamente.',
+        );
+      })
+      .catch(() =>
+        Alert.alert(
+          'Error',
+          'Surgio un pequeño inconveniente, vuelve a intentarlo de nuevo.',
+        ),
+      );
+  };
+
+  // <----- STATE ------>
+  const state = {
+    modal: {
+      modalIsOpen,
+      setModalIsOpen,
     },
+    animal,
+    setAnimal,
+
+    animals,
+    setAnimals,
+
+    firebase: {
+      newAnimal,
+      newProfit,
+      newExpense,
+      getAnimals,
+    },
+
+    categories,
+    setCategories,
   };
 
   return <AppContext.Provider value={state}>{children}</AppContext.Provider>;
